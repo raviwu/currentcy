b4d5f11fe2874c6338c4a7da30765602d2ea55c8
Ravi Wu <raviwu@gmail.com>
2020-08-06 22:57:54 +0800

    Cache the data fetch to tempFile
    To reduce the request times, will check the currentcy data in TempDir.
    
    Read the tempFile line by line.
    
    The CSV response is having incorrect fields, so use the plaintext URL
    request instead.

--- cmd/currentcy/main.go
+++ cmd/currentcy/main.go
@@ -1,35 +1,63 @@
 package main
 
 import (
-	"encoding/csv"
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-var dataSource = "https://rate.bot.com.tw/xrt/flcsv/0/day"
-
-func readCSVFromURL(url string) ([][]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	reader := csv.NewReader(resp.Body)
-	reader.Comma = ';'
-	data, err := reader.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
+func init() {
+	cacheCurrentcyFilePath = fmt.Sprintf("%scacheCurrentcy-%d-%d-%d", os.TempDir(), time.Now().Year(), time.Now().Month(), time.Now().Day())
 }
 
+var cacheCurrentcyFilePath = ""
+var dataSource = "https://rate.bot.com.tw/xrt/fltxt/0/day?Lang=en-US"
+
 func main() {
-	data, err := readCSVFromURL(dataSource)
+	if fileNotExists(cacheCurrentcyFilePath) {
+		saveRequestToFile(dataSource, cacheCurrentcyFilePath)
+	}
+
+	cacheFile, err := os.Open(cacheCurrentcyFilePath)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	defer cacheFile.Close()
+
+	fileScanner := bufio.NewScanner(cacheFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileTextLines []string
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	for _, eachline := range fileTextLines {
+		fmt.Println(eachline)
+	}
+}
+
+func fileNotExists(name string) bool {
+	_, err := os.Stat(name)
+	return os.IsNotExist(err)
+}
+
+func saveRequestToFile(url string, file string) {
+	cacheFile, err := os.Create(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(data)
+	defer cacheFile.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(cacheFile, resp.Body)
 }
